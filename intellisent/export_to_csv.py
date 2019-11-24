c1a32c305229bcdcc7e0756d98c74d6358480e99
surajanbumani <37396185+surajanbumani@users.noreply.github.com>
2019-11-24 11:57:49 +0530

    fixed html issues

--- intellisent/export_to_csv.py
+++ intellisent/export_to_csv.py
@@ -1,6 +1,7 @@
 import sqlite3
 import pandas as pd
 
+# connection to sqlLite DB
 conn = sqlite3.connect('db.sqlite3')
 
 df = pd.read_sql_query('select * from sent_backend_tweet;', conn);
@@ -8,3 +9,5 @@ df = pd.read_sql_query('select * from sent_backend_tweet;', conn);
 df.to_csv('tweets_new.csv')
 
 conn.close()
+
+
